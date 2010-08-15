2d6bd761521e55741df3bdfe8a50ca7365b73894
Koushik Dutta <koushd@gmail.com>
2010-08-15 01:56:20 -0700

    working .32 kernel

--- extract-files.sh
+++ extract-files.sh
@@ -18,6 +18,8 @@ DEVICE=inc
 
 mkdir -p ../../../vendor/htc/$DEVICE/proprietary
 
+adb pull /system/lib/hw/gralloc.qsd8k.so ../../../vendor/htc/$DEVICE/proprietary
+
 adb pull /system/bin/akmd ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/snd ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/snd8k ../../../vendor/htc/$DEVICE/proprietary
@@ -29,7 +31,7 @@ adb pull /system/etc/AudioBTID.csv ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/TPA2018.csv ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/WPDB.zip ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/agps_rm ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/etc/firmware/BCM4329B1_002.002.023.0298.0303.hcd ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/etc/firmware/BCM4329B1_002.002.023.0436.0439.hcd ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/firmware/Incredible_C_20100310_VZW_Neo_324016_v4_PMIC.acdb ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/firmware/Incredible_C_20100319_VZW_Neo_324016_v5_VZW.acdb ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/firmware/Incredible_C_20100319_VZW_Neo_324016_v8_default.acdb ../../../vendor/htc/$DEVICE/proprietary
@@ -85,6 +87,9 @@ PRODUCT_COPY_FILES += \\
     vendor/htc/__DEVICE__/proprietary/libgps.so:obj/lib/libgps.so \\
     vendor/htc/__DEVICE__/proprietary/libcamera.so:obj/lib/libcamera.so
 
+#PRODUCT_COPY_FILES += \\
+#    vendor/htc/__DEVICE__/proprietary/gralloc.qsd8k.so:/system/lib/hw/gralloc.qsd8k.so
+
 # All the blobs necessary for passion
 PRODUCT_COPY_FILES += \\
     vendor/htc/__DEVICE__/proprietary/akmd:/system/bin/akmd \\
@@ -98,7 +103,7 @@ PRODUCT_COPY_FILES += \\
     vendor/htc/__DEVICE__/proprietary/TPA2018.csv:/system/etc/TPA2018.csv \\
     vendor/htc/__DEVICE__/proprietary/WPDB.zip:/system/etc/WPDB.zip \\
     vendor/htc/__DEVICE__/proprietary/agps_rm:/system/etc/agps_rm \\
-    vendor/htc/__DEVICE__/proprietary/BCM4329B1_002.002.023.0298.0303.hcd:/system/etc/firmware/BCM4329B1_002.002.023.0298.0303.hcd \\
+    vendor/htc/__DEVICE__/proprietary/BCM4329B1_002.002.023.0436.0439.hcd:/system/etc/firmware/BCM4329B1_002.002.023.0436.0439.hcd \\
     vendor/htc/__DEVICE__/proprietary/Incredible_C_20100310_VZW_Neo_324016_v4_PMIC.acdb:/system/etc/firmware/Incredible_C_20100310_VZW_Neo_324016_v4_PMIC.acdb \\
     vendor/htc/__DEVICE__/proprietary/Incredible_C_20100319_VZW_Neo_324016_v5_VZW.acdb:/system/etc/firmware/Incredible_C_20100319_VZW_Neo_324016_v5_VZW.acdb \\
     vendor/htc/__DEVICE__/proprietary/Incredible_C_20100319_VZW_Neo_324016_v8_default.acdb:/system/etc/firmware/Incredible_C_20100319_VZW_Neo_324016_v8_default.acdb \\
--- inc.mk
+++ inc.mk
@@ -22,7 +22,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+$(call inherit-product, device/common/gps/gps_us.mk)
 
 ## (1) First, the most specific values, i.e. the aspects that are specific to GSM
 
